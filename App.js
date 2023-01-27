1c907894f375f05e423789000c2e5cf288c0c0f9
jagdishkuri <106979942+jagdishkuri@users.noreply.github.com>
2023-01-27 22:31:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Test from "./Test";
+
+ const App = ()=>{
+  return (
+    <div>
+      <Test key={1}/>
+      <Test key={2}/>
+      <Test key={3}/>
+      <Test key={4}/>
+    {/* <h1>Hello to everyone</h1> */}
+    </div>
+  )
+ };
+export default App;
---
+++ Test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./test.css";
+// const Test=()=>{
+//     return (
+//         div>
+//              <h1 id='testid'>Test class code is here</h1>
+//             {/* <h1> Test component second code is here</h1>   */}
+
+//         </div><
+//     )
+// };
+class Test extends React.Component{
+    render(){
+        return (
+            <div>
+              <h1 id='testid'>Test class code is here</h1>
+            </div>
+        )
+    }
+}
+
+
+ export default Test;
